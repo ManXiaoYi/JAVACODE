31f1940e31fb3286ef951cd6d3d2e3e5f7339bf4
满孝意 <manxiaoyi@huizhaofang.com>
2021-07-16 18:54:11 +0800

    并查集优化

--- 2-02-并查集/src/com/my/union/UnionFind_QU_R.java
+++ 2-02-并查集/src/com/my/union/UnionFind_QU_R.java
@@ -1,5 +1,9 @@
 package com.my.union;
 
+/**
+ * Quick Union - 基于rank的优化
+ *
+ */
 public class UnionFind_QU_R {
 
 }
--- 2-02-并查集/src/com/my/union/UnionFind_QU_S.java
+++ 2-02-并查集/src/com/my/union/UnionFind_QU_S.java
@@ -1,5 +1,9 @@
 package com.my.union;
 
+/**
+ * Quick Union - 基于size的优化
+ *
+ */
 public class UnionFind_QU_S {
 
 }
